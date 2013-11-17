067fee00285de8d69cc0256e131426503d8e9ff0
unknown <capelle.mikael@gmail.com>
2013-11-17 17:54:51 +0100

    Modification of SblockGeneration.

--- matlab/SblockGeneration.m
+++ matlab/SblockGeneration.m
@@ -3,16 +3,17 @@
 
 def = legacy_code('initialize')
 % Nom du fichier et du header
-def.SourceFiles = {'print2file.c'};
-def.HeaderFiles = {'print2file.h'};
+def.SourceFiles = {'blocks/led/led.c'};
+def.HeaderFiles = {'led.h'};
+def.IncPaths = {'blocks\led', '..\paparazzi\include'};
 %def.TargetLibFiles = {'ws2_32.lib'};
 %def.HostLibFiles = {'-lwsock32'};
 %def.TargetLibFiles = {'-lws2_32'};
 
 % Nom de la fonction
-def.SFunctionName = 'ex_sfun_print2file';
+def.SFunctionName = 'ex_sfun_led';
 % Les entrées sont y1, y2... etc, les entrées sont u1, u2...etc
-def.OutputFcnSpec = 'void print2function(int16 u1)';
+def.OutputFcnSpec = 'void led(int8 u1, uint8 p1)';
 
 %YOLO
 legacy_code('sfcn_cmex_generate', def);
