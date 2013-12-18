cedaa0dcf3593475effb846ec13c3ec9a0b40ac7
Arnaud Henric <arnaud.henric@gmail.com>
2013-12-18 09:31:08 +0100

    Résolution conflit code bloc LED

--- matlab/targets/ardrone/blocks/led/led.cpp
+++ matlab/targets/ardrone/blocks/led/led.cpp
@@ -7,12 +7,7 @@ void LED_Initialization ()
 #endif
 }
 
-<<<<<<< HEAD
-void LED_Set (int couleur, int led)
-{
-=======
 void LED_Set (int color, int led) {
->>>>>>> cd56867bccda67bc3ea9913e6d148eefbb7176fe
 #ifndef MATLAB_MEX_FILE
     Actuators::Led::set (led, color) ;
 #endif
--- matlab/targets/ardrone/blocks/led/led.h
+++ matlab/targets/ardrone/blocks/led/led.h
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 #ifndef _LED_H
 #define _LED_H
-=======
-#ifndef __PRINT2FILE_H
-#define __PRINT2FILE_H
->>>>>>> cd56867bccda67bc3ea9913e6d148eefbb7176fe
 
 #ifndef MATLAB_MEX_FILE
 
@@ -12,14 +7,8 @@
 
 #endif
 
-<<<<<<< HEAD
-void LED_Initialization();
-
-void LED_Set(int couleur, int led);
-=======
 void LED_Initialization () ;
 
 void LED_Set(int color, int led);
->>>>>>> cd56867bccda67bc3ea9913e6d148eefbb7176fe
 
 #endif
