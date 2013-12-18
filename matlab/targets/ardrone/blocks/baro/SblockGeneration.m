03545ee5d3db6d5d60e43206ec22336dbceeef90
Arnaud Henric <arnaud.henric@gmail.com>
2013-12-18 09:27:31 +0100

    nettoyage fichiers générés

--- matlab/targets/ardrone/blocks/baro/SblockGeneration.m
+++ matlab/targets/ardrone/blocks/baro/SblockGeneration.m
@@ -2,18 +2,18 @@
 
 def = legacy_code('initialize')
 % Nom du fichier et du header
-def.SourceFiles = {'led.cpp'};
-def.HeaderFiles = {'led.h'};
+def.SourceFiles = {'height.c'};
+def.HeaderFiles = {'height.h'};
 def.IncPaths = {'.',};
 
 % Nom de la fonction
-def.SFunctionName = 'ARDrone_LED';
+def.SFunctionName = 'ARDrone_Height';
 % Les sorties sont y1, y2... etc, 
 % les entrées sont u1, u2...etc
 % les paramètres sont p1, p2 ...
 def.Options.language = 'C++';
-def.StartFcnSpec = 'LED_Initialization()';
-def.OutputFcnSpec = 'void LED_Set(int8 u1, uint8 p1)';
+def.StartFcnSpec = 'void Height_Initialization(void)';
+def.OutputFcnSpec = 'int16 y1 Height_Get (void)';
 
 %YOLO
 legacy_code('sfcn_cmex_generate', def);
