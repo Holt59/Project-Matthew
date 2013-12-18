9da96aa5c9e398de9aa65a1608cf1a6816377efc
Arnaud Henric <arnaud.henric@gmail.com>
2013-12-18 14:51:52 +0100

    saturation logicielle pwm moteur

--- matlab/targets/ardrone/blocks/motor/motor.cpp
+++ matlab/targets/ardrone/blocks/motor/motor.cpp
@@ -10,6 +10,10 @@ void Motor_Initialization ()
 void Motor_Set (float pwm, uint8_t motor)
 {
 #ifndef MATLAB_MEX_FILE
+	if (pwm > 100.0)
+		pwm = 100.0;
+	if (pwm < 0.0)
+		pwm = 0.0;
     Actuators::setPWM (motor, (int16_t)((0x1ff*pwm)/100)) ;
 #endif
 }
