116471483351d0a94597ee3c00fafc9b772b7a03
unknown <capelle.mikael@gmail.com>
2014-01-14 10:07:20 +0100

    Update Navdata.cpp

--- Paparazzi-CPP/src/Navdata.cpp
+++ Paparazzi-CPP/src/Navdata.cpp
@@ -461,7 +461,7 @@ namespace Navdata {
 
         namespace Accelerometer {
 
-            const float Sensitivity[3] = {19.5, 19.5, 19.5} ;
+            const float Sensitivity[3] = {0.001953125, 0.001953125, 0.001953125} ;
 			const float Neutral[3] = {2048.0, 2048.0, 2048.0} ;
             
             int16_t getRawX () {
@@ -492,6 +492,9 @@ namespace Navdata {
 
         namespace Magnetometer {
 
+			const float Sensitivity[3] = {0.0071492608, 0.0070882491, 0.0074057781} ;
+			const float Neutral[3] = {118.0, -65.0, 110.0} ;
+
 			const float Bias[3] = {0.253407, 13.703188, 235.300088} ;
 			const float A[3][3] = {
 				{0.0083769, 0.0001547, 0.0001820},
@@ -514,9 +517,8 @@ namespace Navdata {
 					}
 				}
 				
-				for (int i=0 ; i<3; i++) {
-					magnetometer[i] = - magnetometer[i] ;
-				}
+				magnetometer[0] = - magnetometer[0] ;
+				magnetometer[2] = - magnetometer[2] ;
 			
 			}
 
@@ -585,7 +587,146 @@ namespace Navdata {
         void setKp (float kp) { Kp = kp ; }
         void setKi (float ki) { Ki = ki ; }
 
+		float b_x = 1, b_z = 0; // reference direction of flux in earth frame
+
         void update () {
+
+			float gx = IMU::gyroscope[0],
+                gy = IMU::gyroscope[1],
+                gz = IMU::gyroscope[2],
+                ax = IMU::accelerometer[0],
+                ay = IMU::accelerometer[1],
+                az = IMU::accelerometer[2],
+                mx = IMU::magnetometer[0],
+                my = IMU::magnetometer[1],
+                mz = IMU::magnetometer[2] ;
+
+			float norm; // vector norm
+			float SEqDot_omega_1, SEqDot_omega_2, SEqDot_omega_3, SEqDot_omega_4; // quaternion rate from gyroscopes elements
+			float f_1, f_2, f_3, f_4, f_5, f_6; // objective function elements
+			float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33, // objective function Jacobian elements
+			J_41, J_42, J_43, J_44, J_51, J_52, J_53, J_54, J_61, J_62, J_63, J_64; //
+			float SEqHatDot_1, SEqHatDot_2, SEqHatDot_3, SEqHatDot_4; // estimated direction of the gyroscope error
+			float w_err_x, w_err_y, w_err_z; // estimated direction of the gyroscope error (angular)
+			float h_x, h_y, h_z; // computed flux in the earth frame
+			// axulirary variables to avoid reapeated calcualtions
+			float halfSEq_1 = 0.5f * quaternion[0];
+			float halfSEq_2 = 0.5f * quaternion[1];
+			float halfSEq_3 = 0.5f * quaternion[2];
+			float halfSEq_4 = 0.5f * quaternion[3];
+			float twoSEq_1 = 2.0f * quaternion[0];
+			float twoSEq_2 = 2.0f * quaternion[1];
+			float twoSEq_3 = 2.0f * quaternion[2];
+			float twoSEq_4 = 2.0f * quaternion[3];
+			float twob_x = 2.0f * b_x;
+			float twob_z = 2.0f * b_z;
+			float twob_xSEq_1 = 2.0f * b_x * quaternion[0];
+			float twob_xSEq_2 = 2.0f * b_x * quaternion[1];
+			float twob_xSEq_3 = 2.0f * b_x * quaternion[2];
+			float twob_xSEq_4 = 2.0f * b_x * quaternion[3];
+			float twob_zSEq_1 = 2.0f * b_z * quaternion[0];
+			float twob_zSEq_2 = 2.0f * b_z * quaternion[1];
+			float twob_zSEq_3 = 2.0f * b_z * quaternion[2];
+			float twob_zSEq_4 = 2.0f * b_z * quaternion[3];
+			float SEq_1SEq_2;
+			float SEq_1SEq_3 = quaternion[0] * quaternion[2];
+			float SEq_1SEq_4;
+			float SEq_2SEq_3;
+			float SEq_2SEq_4 = quaternion[1] * quaternion[3];
+			float SEq_3SEq_4;
+			float twom_x = 2.0f * mx;
+			float twom_y = 2.0f * my;
+			float twom_z = 2.0f * mz;
+			// normalise the accelerometer measurement
+			norm = sqrt(ax * ax + ay * ay + az * az);
+			ax /= norm;
+			ay /= norm;
+			az /= norm;
+			// normalise the magnetometer measurement
+			norm = sqrt(mx * mx + my * my + mz * mz);
+			mx /= norm;
+			my /= norm;
+			mz /= norm;
+			// compute the objective function and Jacobian
+			f_1 = twoSEq_2 * quaternion[3] - twoSEq_1 * quaternion[2] -ax;
+			f_2 = twoSEq_1 * quaternion[1] + twoSEq_3 * quaternion[3] - ay;
+			f_3 = 1.0f - twoSEq_2 * quaternion[1] - twoSEq_3 * quaternion[2] - az;
+			f_4 = twob_x * (0.5f - quaternion[2] * quaternion[2] - quaternion[3] * quaternion[3]) + twob_z * (SEq_2SEq_4 - SEq_1SEq_3) - mx;
+			f_5 = twob_x * (quaternion[1] * quaternion[2] - quaternion[0] * quaternion[3]) + twob_z * (quaternion[0] * quaternion[1] + quaternion[2] * quaternion[3]) - my;
+			f_6 = twob_x * (SEq_1SEq_3 + SEq_2SEq_4) + twob_z * (0.5f - quaternion[1] * quaternion[1] - quaternion[2] * quaternion[2]) - mz;
+			J_11or24 = twoSEq_3; // J_11 negated in matrix multiplication
+			J_12or23 = 2.0f * quaternion[3];
+			J_13or22 = twoSEq_1; // J_12 negated in matrix multiplication
+			J_14or21 = twoSEq_2;
+			J_32 = 2.0f * J_14or21; // negated in matrix multiplication
+			J_33 = 2.0f * J_11or24; // negated in matrix multiplication
+			J_41 = twob_zSEq_3; // negated in matrix multiplication
+			J_42 = twob_zSEq_4;
+			J_43 = 2.0f * twob_xSEq_3 + twob_zSEq_1; // negated in matrix multiplication
+			J_44 = 2.0f * twob_xSEq_4 - twob_zSEq_2; // negated in matrix multiplication
+			J_51 = twob_xSEq_4 - twob_zSEq_2; // negated in matrix multiplication
+			J_52 = twob_xSEq_3 + twob_zSEq_1;
+			J_53 = twob_xSEq_2 + twob_zSEq_4;
+			J_54 = twob_xSEq_1 - twob_zSEq_3; // negated in matrix multiplication
+			J_61 = twob_xSEq_3;
+			J_62 = twob_xSEq_4 - 2.0f * twob_zSEq_2;
+			J_63 = twob_xSEq_1 - 2.0f * twob_zSEq_3;
+			J_64 = twob_xSEq_2;
+			// compute the gradient (matrix multiplication)
+			SEqHatDot_1 = J_14or21 * f_2 - J_11or24 * f_1 - J_41 * f_4 - J_51 * f_5 + J_61 * f_6;
+			SEqHatDot_2 = J_12or23 * f_1 + J_13or22 * f_2 - J_32 * f_3 + J_42 * f_4 + J_52 * f_5 + J_62 * f_6;
+			SEqHatDot_3 = J_12or23 * f_2 - J_33 * f_3 - J_13or22 * f_1 - J_43 * f_4 + J_53 * f_5 + J_63 * f_6;
+			SEqHatDot_4 = J_14or21 * f_1 + J_11or24 * f_2 - J_44 * f_4 - J_54 * f_5 + J_64 * f_6;
+			// normalise the gradient to estimate direction of the gyroscope error
+			norm = sqrt(SEqHatDot_1 * SEqHatDot_1 + SEqHatDot_2 * SEqHatDot_2 + SEqHatDot_3 * SEqHatDot_3 + SEqHatDot_4 * SEqHatDot_4);
+			SEqHatDot_1 = SEqHatDot_1 / norm;
+			SEqHatDot_2 = SEqHatDot_2 / norm;
+
+			SEqHatDot_3 = SEqHatDot_3 / norm;
+			SEqHatDot_4 = SEqHatDot_4 / norm;
+			// compute angular estimated direction of the gyroscope error
+			w_err_x = twoSEq_1 * SEqHatDot_2 - twoSEq_2 * SEqHatDot_1 - twoSEq_3 * SEqHatDot_4 + twoSEq_4 * SEqHatDot_3;
+			w_err_y = twoSEq_1 * SEqHatDot_3 + twoSEq_2 * SEqHatDot_4 - twoSEq_3 * SEqHatDot_1 - twoSEq_4 * SEqHatDot_2;
+			w_err_z = twoSEq_1 * SEqHatDot_4 - twoSEq_2 * SEqHatDot_3 + twoSEq_3 * SEqHatDot_2 - twoSEq_4 * SEqHatDot_1;
+			// compute and remove the gyroscope baises
+			intErrors[0] += w_err_x * samplePeriod * Ki;
+			intErrors[1] += w_err_y * samplePeriod * Ki;
+			intErrors[2] += w_err_z * samplePeriod * Ki;
+			gx -= intErrors[0];
+			gy -= intErrors[1];
+			gz -= intErrors[2];
+			// compute the quaternion rate measured by gyroscopes
+			SEqDot_omega_1 = -halfSEq_2 * gx - halfSEq_3 * gy - halfSEq_4 * gz;
+			SEqDot_omega_2 = halfSEq_1 * gx + halfSEq_3 * gz - halfSEq_4 * gy;
+			SEqDot_omega_3 = halfSEq_1 * gy - halfSEq_2 * gz + halfSEq_4 * gx;
+			SEqDot_omega_4 = halfSEq_1 * gz + halfSEq_2 * gy - halfSEq_3 * gx;
+			// compute then integrate the estimated quaternion rate
+			quaternion[0] += (SEqDot_omega_1 - (Kp * SEqHatDot_1)) * samplePeriod;
+			quaternion[1] += (SEqDot_omega_2 - (Kp * SEqHatDot_2)) * samplePeriod;
+			quaternion[2] += (SEqDot_omega_3 - (Kp * SEqHatDot_3)) * samplePeriod;
+			quaternion[3] += (SEqDot_omega_4 - (Kp * SEqHatDot_4)) * samplePeriod;
+			// normalise quaternion
+			norm = sqrt(quaternion[0] * quaternion[0] + quaternion[1] * quaternion[1] + quaternion[2] * quaternion[2] + quaternion[3] * quaternion[3]);
+			quaternion[0] /= norm;
+			quaternion[1] /= norm;
+			quaternion[2] /= norm;
+			quaternion[3] /= norm;
+			// compute flux in the earth frame
+			SEq_1SEq_2 = quaternion[0] * quaternion[1]; // recompute axulirary variables
+			SEq_1SEq_3 = quaternion[0] * quaternion[2];
+			SEq_1SEq_4 = quaternion[0] * quaternion[3];
+			SEq_3SEq_4 = quaternion[2] * quaternion[3];
+			SEq_2SEq_3 = quaternion[1] * quaternion[2];
+			SEq_2SEq_4 = quaternion[1] * quaternion[3];
+			h_x = twom_x * (0.5f - quaternion[2] * quaternion[2] - quaternion[3] * quaternion[3]) + twom_y * (SEq_2SEq_3 - SEq_1SEq_4) + twom_z * (SEq_2SEq_4 + SEq_1SEq_3);
+			h_y = twom_x * (SEq_2SEq_3 + SEq_1SEq_4) + twom_y * (0.5f - quaternion[1] * quaternion[1] - quaternion[3] * quaternion[3]) + twom_z * (SEq_3SEq_4 - SEq_1SEq_2);
+			h_z = twom_x * (SEq_2SEq_4 - SEq_1SEq_3) + twom_y * (SEq_3SEq_4 + SEq_1SEq_2) + twom_z * (0.5f - quaternion[1] * quaternion[1] - quaternion[2] * quaternion[2]);
+			// normalise the flux vector to have only components in the x and z
+			b_x = sqrt((h_x * h_x) + (h_y * h_y));
+			b_z = h_z;
+
+
+			/*
 
             float q1 = quaternion[0], q2 = quaternion[1], q3 = quaternion[2], q4 = quaternion[3];   // short name local variable for readability
             float norm;
@@ -688,7 +829,7 @@ namespace Navdata {
             quaternion[0] = q1 * norm;
             quaternion[1] = q2 * norm;
             quaternion[2] = q3 * norm;
-            quaternion[3] = q4 * norm;
+            quaternion[3] = q4 * norm;*/
         }
 
         struct EulerAngles getEulerAngles () {
