d68f9ca219e9a7f61ca9d5532db9376b511118e7
Holt59 <capelle.mikael@gmail.com>
2014-01-16 21:27:26 +0100

    Add gyroscope.cpp to compute offset and noise from gyroscope (not compiled yet).

---
+++ Paparazzi-CPP/calibration/gyroscope.cpp
@@ -0,0 +1,80 @@
+#include "../include/paparazzi/Navdata.h"
+
+#include <iostream>
+#include <algorithm>
+#include <unistd.h>
+
+const int NB_SAMPLES = 10000 ;
+
+int main (int argc, char *argv[]) {
+
+    struct {
+        int16_t x, y, z ;
+    } samples[NB_SAMPLES] = {0} ;
+
+    if (!Navdata::init ()) {
+        std::cerr << "Error initializing navdata." << std::endl ;
+        return 1 ;
+    }
+
+    for (int i = 0 ; i < NB_SAMPLES ; ++i) {
+        samples[i] = {
+            Navdata::IMU::Gyroscope::getRawX (),
+            Navdata::IMU::Gyroscope::getRawY (),
+            Navdata::IMU::Gyroscope::getRawZ ()
+        } ;
+        usleep (2000) ;
+    }
+
+    int32_t offX = 0, offY = 0, offZ = 0 ;
+
+    for (int i = 0; i < NB_SAMPLES ; ++i) {
+        offX += samples[i].x ;
+        offY += samples[i].y ;
+        offZ += samples[i].z ;
+    }
+
+    offX /= NB_SAMPLES ;
+    offY /= NB_SAMPLES ;
+    offZ /= NB_SAMPLES ;
+
+    struct {
+        struct {
+            int16_t min ;
+            int16_t max ;
+            int32_t avg ;
+        } x, y, z ;
+    } noises = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    for (int i = 0; i < NB_SAMPLES; i++) {
+
+        int16_t noiseX = (int16_t) (samples[i].x - offX) ;
+        int16_t absNoiseX = (int16_t) abs (noiseX) ;
+        noises.x.min = std::min (noises.x.min, absNoiseX) ;
+        noises.x.max = std::max (noises.x.max, absNoiseX) ;
+        noises.x.avg += noiseX ;
+
+        int16_t noiseY = (int16_t) (samples[i].y - offY) ;
+        int16_t absNoiseY = (int16_t) abs (noiseY) ;
+        noises.y.min = std::min (noises.y.min, absNoiseY) ;
+        noises.y.max = std::max (noises.y.max, absNoiseY) ;
+        noises.y.avg += noiseY ;
+
+        int16_t noiseZ = (int16_t) (samples[i].z - offZ) ;
+        int16_t absNoiseZ = (int16_t) abs (noiseZ) ;
+        noises.z.min = std::min (noises.z.min, absNoiseZ) ;
+        noises.z.max = std::max (noises.z.max, absNoiseZ) ;
+        noises.z.avg += noiseZ ;
+    }
+
+    noises.z.avg /= NB_SAMPLES ;
+    noises.x.avg /= NB_SAMPLES ;
+    noises.y.avg /= NB_SAMPLES ;
+
+    return 0 ;
+
+}
